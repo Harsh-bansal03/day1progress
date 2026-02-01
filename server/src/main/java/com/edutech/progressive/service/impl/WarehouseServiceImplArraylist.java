ae844507ecb6f375fac6d56f8a4a90e1c28c1b33
Harsh-bansal03 <dummy@gmail.com>
2026-02-01 14:52:17 +0000

    solve initial commit

--- server/src/main/java/com/edutech/progressive/service/impl/WarehouseServiceImplArraylist.java
+++ server/src/main/java/com/edutech/progressive/service/impl/WarehouseServiceImplArraylist.java
@@ -11,7 +11,7 @@ import java.util.List;
 import com.edutech.progressive.entity.Warehouse;
 import com.edutech.progressive.service.WarehouseService;
 
-public class WarehouseServiceImplArrayList implements WarehouseService {
+public class WarehouseServiceImplArraylist implements WarehouseService {
 
     private List<Warehouse> warehouses = new ArrayList<>();
 
