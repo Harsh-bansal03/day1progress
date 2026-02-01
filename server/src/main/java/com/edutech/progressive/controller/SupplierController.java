2894942dea6277b8483e5159dc771115f37562dc
Kunal Bansal <115614469+Harsh-bansal03@users.noreply.github.com>
2026-02-01 20:34:57 +0530

    Add RESTful endpoints to SupplierController

--- server/src/main/java/com/edutech/progressive/controller/SupplierController.java
+++ server/src/main/java/com/edutech/progressive/controller/SupplierController.java
@@ -2,39 +2,53 @@ package com.edutech.progressive.controller;
 
 import com.edutech.progressive.entity.Supplier;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/suppliers")
 public class SupplierController {
 
+    @GetMapping
     public ResponseEntity<List<Supplier>> getAllSuppliers() {
         return null;
     }
 
-    public ResponseEntity<Supplier> getSupplierById(int supplierId) {
+    @GetMapping("/{supplierId}")
+    public ResponseEntity<Supplier> getSupplierById(@PathVariable int supplierId) {
         return null;
     }
 
-    public ResponseEntity<Integer> addSupplier(Supplier supplier) {
+    @PostMapping
+    public ResponseEntity<Integer> addSupplier(@RequestBody Supplier supplier) {
         return null;
     }
 
-    public ResponseEntity<Void> updateSupplier(Supplier supplier) {
+    @PutMapping("/{supplierId}")
+    public ResponseEntity<Void> updateSupplier(
+            @PathVariable int supplierId,
+            @RequestBody Supplier supplier) {
         return null;
     }
 
-    public ResponseEntity<Void> deleteSupplier(int supplierId) {
+    @DeleteMapping("/{supplierId}")
+    public ResponseEntity<Void> deleteSupplier(@PathVariable int supplierId) {
         return null;
     }
 
+    @GetMapping("/arraylist")
     public ResponseEntity<List<Supplier>> getAllSuppliersFromArrayList() {
         return null;
     }
 
-    public ResponseEntity<Integer> addSupplierToArrayList(Supplier supplier) {
+    @PostMapping("/arraylist")
+    public ResponseEntity<Integer> addSupplierToArrayList(
+            @RequestBody Supplier supplier) {
         return null;
     }
 
+    @GetMapping("/arraylist/sorted")
     public ResponseEntity<List<Supplier>> getAllSuppliersSortedByNameFromArrayList() {
         return null;
     }
